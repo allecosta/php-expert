3975f69f683eb553cc711168a770c3051ab4a1ed
allecosta <alle_costa10@outlook.com>
2022-11-10 09:22:20 -0300

    math

---
+++ 01-basic/05_math.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Função pi() - Retorna o valor de PI
+ * 
+ */
+echo pi() . PHP_EOL;
+
+
+/**
+ * Função min() e max() - Utilizadas para encontrar o menor e maior valor, respectivamente, em uma lista de argumentos
+ * 
+ */
+echo (min(1, 12, 25, 19, 83, -38)) . PHP_EOL;
+
+echo (max(1, 12, 25, 19, 83, -38)) . PHP_EOL;
+
+
+/**
+ * Função abs() - Retona o valor absoluto (positivo) de um número
+ * 
+ */
+echo (abs(-5.9)) . PHP_EOL;
+
+
+/**
+ * Função sqrt() - Retorna a raiz quadrada de um número
+ * 
+ */
+echo (sqrt(64)) . PHP_EOL;
+
+
+/**
+ * Função round() - Arrendonda um número de ponto fluante para um valor (inteiro) mais proximo
+ * 
+ */
+echo (round(0.83)) . PHP_EOL;
+
+echo(round(0.19)) . PHP_EOL;
+
+
+/**
+ * Função rand() - Gera valor/es aleatorios
+ * 
+ */
+echo (rand()) . PHP_EOL;
+
+echo (rand(0, 100));
