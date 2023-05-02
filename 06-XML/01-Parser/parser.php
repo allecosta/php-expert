cfef3a768401f1ffbefa5be333ba168c5fb4f755
allecosta <alle_costa10@outlook.com>
2023-05-02 09:41:02 -0300

    SimpleXML Parser

---
+++ 06-XML/01-Parser/parser.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * A linguagem XML é uma forma de estruturar dados para compartilhamento entre sites na web
+ * Tecnologias como RSS feed e Podcasts são escritas em XML
+ * 
+ * simpleXML é uma extensão PHP que nos permite manipular e obter dados XML.
+ */
+
+ $xmlData = "<?xml version='1.0' encoding='utf-8' ?>
+    <note>
+        <to>João</to>
+        <from>Maria</from>
+        <heading>Lembre-se</heading>
+        <body>Estaremos juntos nestes novos desafios!</body>
+    </note>";
+
+$xml = simplexml_load_string($xmlData) or die ("OPS! Não foi possível criar objeto.");
+
+print_r($xml);
+
+
+// Verificando erros  
+libxml_use_internal_errors(true);
+
+$xmlData = "<?xml version='1.0' encoding='utf-8' ?>
+    <document>
+        <user>João</u_ser>
+        <email>joao@hostmail.com</e_mail>
+    </document>";
+
+$xml = simplexml_load_string($xmlData);
+
+if ($xml === false) {
+    echo "Falha no carregamento do XML" . PHP_EOL;
+
+    foreach (libxml_get_errors() as $error) {
+        echo $error->message . PHP_EOL;
+    }
+} else {
+    print_r($xml);
+}
+
+
+// Lendo informações do arquivo xfile.xml
+$xml = simplexml_load_file("xfile.xml") or die ("OPS! Não foi possível criar objeto.");
+
+print_r($xml);
